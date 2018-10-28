3885f4614710b7ed1b38cd7aaaffbc4ec9f5aac4
mkjmdski <mikolaj.mlodzikowski@gmail.com>
2018-10-28 14:05:59 +0100

    Try lambda

--- build_dist.sh
+++ build_dist.sh
@@ -3,11 +3,13 @@ container="dist"
 image="hugo:generator"
 
 docker build \
-    --build-arg baseURL="https://mlodzikowski.pl" \
     --tag $image \
     --target generator \
     site
-docker run -itd --name $container $image
-docker cp $container:/site/public .
-docker kill $container > /dev/null
-docker rm $container > /dev/null
+
+docker run -it \
+    --env baseURL="https://mlodzikowski.pl" \
+    --volume $PWD/site:/site \
+    --volume $PWD/production/dist:/site/public \
+    --name $container \
+    $image bash build.sh
