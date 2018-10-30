b62d3cc8b02f6a07ac9b5109194f37c32c620016
mkjmdski <mikolaj.mlodzikowski@gmail.com>
2018-10-30 13:37:41 +0100

    Change main template

--- site/build.sh
+++ site/build.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
-./build_indexes.py
-for module in main presentations; do (
-    cd $module
-    if [ "$module" != "main" ]; then
-        baseURL="$baseURL/$module"
-    fi
-    hugo --baseURL="$baseURL"
-) done
+
+function generate_file {
+    output="$1"
+    title="$2"
+    weight="$3"
+    (
+        export title weight
+        envsubst < /site/hack_index.template > $output
+    )
+}
+
+function build_hugo {
+    modules=(main presentations)
+    for module in ${modules[@]}; do (
+        cd $module
+        echo "Building $module..."
+        if [ "$module" != "main" ]; then
+            baseURL="$baseURL/$module"
+        fi
+        hugo --baseURL="$baseURL/"
+    ) done
+}
+
+function symlink_presentations {
+    weight=10
+    presentation_root="/site/presentations/content"
+    main_root="/site/main/content/presentations"
+    find $presentation_root -maxdepth 1 -type d | \
+        while read presentation; do
+            if [ "$presentation" != "$presentation_root" ]; then
+                pres_name="$(echo $presentation | rev | cut -d'/' -f 1 | rev)"
+                generate_file "$main_root/$pres_name.md" $pres_name $weight
+                let "weight+=10"
+            fi
+        done
+}
+
+function main {
+    symlink_presentations
+    build_hugo
+}
+
+main
---
+++ site/main/archetypes/post.md
@@ -0,0 +1,12 @@
+---
+title: "{{ replace .Name "-" " " | title }}"
+date: {{ .Date }}
+draft: true
+linktitle: "{{ replace .Name "-" " " | title }}"
+# menu:
+#   main:
+#     parent: choose-parent
+# next: /choose/next
+# prev: /choose/prev
+weight: 10
+---
