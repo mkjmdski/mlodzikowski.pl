de73037dc96742abe298a58bce6dde7331a52c96
mkjmdski <mikolaj.mlodzikowski@gmail.com>
2018-10-30 18:01:32 +0100

    Use my data for CV

--- builds/build_cv.sh
+++ builds/build_cv.sh
@@ -1,8 +1,9 @@
-# #!/bin/bash
-# image="thomasweise/texlive"
+#!/bin/bash
+image="thomasweise/texlive"
 
-# docker run -it \
-#     --volume $PWD/cv:/doc \
-#     $image make
+docker run -it \
+    --volume $PWD/cv:/doc \
+    $image make
 
-# mv cv/examples/cv.pdf production
+if [ -f "production/cv.pdf" ]; then rm production/cv.pdf; fi
+mv cv/examples/cv.pdf production
